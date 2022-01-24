3841e32fd0ff1b9b5999d3617bcb95445dd349a3
mpdev2311 <marcospdev2311gmail.com>
2022-01-24 18:51:10 -0300

    construtor primitivo.js

---
+++ construtor primitivo.js
@@ -0,0 +1,15 @@
+console.log(typeof true); // Boolean
+console.log(typeof Boolean(true));// Boolean
+console.log(typeof new Boolean(true)); // Boolean
+console.log(typeof 'Gabriel'); // Boolean
+console.log(typeof 28); // Boolean
+
+console.log('Gabriel'. length);
+
+var doze = new Number(12);
+var quinze = doze +3;
+
+console.log(quinze);
+
+console.log(typeof doze);
+console.log(typeof quinze);
