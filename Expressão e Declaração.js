7a2f9a650b606ec3292232c93ce3c07bae5ec7d0
mpdev2311 <marcospdev2311gmail.com>
2022-01-25 18:32:54 -0300

    Expressão e Declaração.js

---
+++ Expressão e Declaração.js
@@ -0,0 +1,24 @@
+console.log(1 + 1);
+console.log(Math.random() + 5);
+
+function expressão() {
+  return 1+1;
+}
+
+console.log(expressão());
+
+ var variavel = 20;
+
+// if (true) {
+//  variavel = 30;
+// }
+
+function soma(a) {
+  return a + 10;
+}
+
+console.log(soma(Math.random()));
+
+//const teste() {
+//  variavel = 30;  
+//}
