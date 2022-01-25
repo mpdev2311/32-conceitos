187aa04c5ff17f3e4fbd3e76b1044d621a9cf75e
mpdev2311 <marcospdev2311gmail.com>
2022-01-25 13:07:36 -0300

    Implicito,Explicito e chmadas de métodos.js

---
+++ Implícito,Explicito e chamadas de métodos.js
@@ -0,0 +1,22 @@
+ // COERÇÂO
+ // console.log('5' -5);
+ // console.log('5' + 5);
+ // console.log(true + 1);
+ // console.log(true + true);
+ // console.log([] + {});
+ // console.log([] + []);
+
+ // IMPLICITO
+// console.log(+'5');
+// console.log(5 + '');
+// console.log(123 && 'oi');
+// console.log(null || true);
+
+// EXPLICITO
+// console.log(Number('50'));
+// console.log(String(50));
+
+/*
+Public integer somaNumeros(integer a, integer b) { return a + b}
+function somaNumeros(a, b) { return a + b; }
+*/
