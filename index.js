4447444773c08ae92f0b7c162fdd3322a3b1d912
mpdev2311 <marcospdev2311gmail.com>
2022-01-25 20:55:32 -0300

    módulos

---
+++ index.js
@@ -0,0 +1,9 @@
+//import { helloWorld as olaMundo , multiplica } from "./utilitarios";
+//helloWorld();
+//olaMundo();
+
+// import * as utilitarios from './utilitarios.js';
+// utilitarios.multiplica(5)
+
+import utilitarios from '.utilitarios.js';
+utilitarios();
---
+++ utilitarios.js
@@ -0,0 +1,13 @@
+const valor = 5;
+
+const helloWorld = function() {
+    alert('olá mundo');
+}
+
+const multiplica = function(x) {
+    alert(x * alert);
+}
+
+// export { multiplica, helloWorld };
+export default helloWorld;
+
