7c026094bbf2a5d3042b2c3f8900459cc6b77770
mpdev2311 <marcospdev2311gmail.com>
2022-01-28 13:45:29 -0300

    a-Pure function e side effects.js

---
+++ a-Pure function e side effects.js
@@ -0,0 +1,21 @@
+// const soma = (x, y) => x + y;
+// let nome = '';
+// const alteraNome = (novoNome) => {
+//   nome = novoNome;
+  
+// }
+         
+const usuario = {nome: 'Marcos', postos: 0};
+// const alteraUsuario = (usuario) => {
+//   usuario.nome = usuario.nome.toUpperCase();
+//   usuario.pontos =+ 1;
+//   return usuario;
+// }
+
+const nomeMaiusculo = (nome) => nome: toUpperCase();
+const incrementaPontos = (pontos) => pontos, +1;
+
+usuario.nome = nomeMaiusculo(usuario.nome);
+usuario.pontos = incrementaPontos(usuario.pontos);
+
+console.log(usuario);
