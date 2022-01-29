be1123ac68aa253c26cb4d15aaf1206c8fecbbc0
mpdev2311 <marcospdev2311gmail.com>
2022-01-29 17:59:29 -0300

    a - Data Structures: Stack e Queue.js

---
+++ a - Data Structur e Stacke Queue.js
@@ -0,0 +1,26 @@
+onst pilha = [];
+pilha.push(0);
+pilha.push(1);
+console.log(pilha);
+pilha.push(2);
+pilha.push(3);
+pilha.pop();
+console.log(pilha);
+
+// const pilha = [];
+// pilha.unshift(0);
+// pilha.unshift(1);
+// console.log(pilha);
+// pilha.unshift(2);
+// pilha.unshift(3);
+// pilha.shift();
+// console.log(pilha);
+
+const fila = [];
+fila.push(0);
+fila.push(1);
+console.log(fila);
+fila.push(2);
+fila.push(3);
+fila.shift();
+console.log(fila);
