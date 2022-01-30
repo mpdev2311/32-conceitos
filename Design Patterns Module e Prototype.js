da8bb836c41a769c8d286ef5973ccdd3d3eaf543
mpdev2311 <marcospdev2311gmail.com>
2022-01-29 21:22:10 -0300

    Design Patterns Module e Prototype.js

---
+++ Design Patterns Module e Prototype.js
@@ -0,0 +1,16 @@
+const Carro = {
+    tracao: '4x4',
+    ligar() {
+      return 'ligou';
+    }
+    
+  }
+  
+  const meuCarro = Object.create(Carro, {dono: { value: 'marcos'}});
+  
+  Carro.__proto__.desligar = function(){
+    return 'desligou';
+  } 
+  
+  console.log(meuCarro.ligar());
+  console.log(meuCarro.desligar());
